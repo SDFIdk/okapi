9631d3184d79c8a230e20e5d960b3ea14f8262a4
Frank Thomsen <frtho@sdfi.dk>
2022-09-05 14:09:15 +0200

    no more visual regression tests for now

--- test/e2e/simple.spec.js
+++ test/e2e/simple.spec.js
@@ -8,8 +8,10 @@ test.describe('Display a map', () => {
   
   test('should allow me to see the map', async ({ page }) => {
     await expect(page.locator('.ol-layer canvas')).toBeTruthy()
-    await page.waitForLoadState('networkidle')
-    await expect(page).toHaveScreenshot({timeout: 1500, maxDiffPixelRatio: 0.02 })
+
+    // Visual regression testing needs some love before production use
+    //await page.waitForLoadState('networkidle')
+    //await expect(page).toHaveScreenshot({timeout: 1500, maxDiffPixelRatio: 0.02 })
   })
   
 })
