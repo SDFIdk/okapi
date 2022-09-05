476e7d756cc25a1a0ea42f91b97a5c023c078d6f
Frank Thomsen <frtho@sdfi.dk>
2022-09-05 15:09:00 +0200

    improve e2e tests

--- test/e2e/advanced.spec.js
+++ test/e2e/advanced.spec.js
@@ -9,4 +9,9 @@ test.describe('Display a map', () => {
   test('should allow me to see the map', async ({ page }) => {
     await expect(page.locator('.ol-layer canvas')).toBeTruthy()
   })
+
+  test('should allow me to toggle the layer switcher', async({ page }) => {
+    await page.click('#layer-switcher-button')
+    await expect(page.locator('.container1 ')).toHaveClass('container1 expanded')
+  })
 })
