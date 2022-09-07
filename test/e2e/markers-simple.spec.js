2783fc4c3c5742ebf82d3789af57dad89b04dc33
Frank Thomsen <frtho@sdfi.dk>
2022-09-07 12:18:32 +0200

    change test target path

--- test/e2e/markers-simple.spec.js
+++ test/e2e/markers-simple.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test')
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('/examples/markers-simple.html')
+  await page.goto('/test/markers-simple.html')
 })
 
 test.describe('Display a map with a single marker', () => {
