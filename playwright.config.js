7bfad0a14f47df5435076c7678d2e3f58f851f17
Frank Thomsen <frank.thomsen@gmail.com>
2023-11-30 19:07:31 +0100

    change playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -32,13 +32,13 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'dot' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.CI ? 'https://sdfidk.github.io/okapi/' : 'localhost:7777',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -100,10 +100,12 @@ const config = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
+  /*
   webServer: {
     command: 'npm run start',
     port: 7777,
   }
+  */
 };
 
 module.exports = config;
